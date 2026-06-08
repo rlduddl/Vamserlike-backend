58cb001adee5c3e717e20537424d4373b6e93c8a
DESKTOP-883EC3D\soldesk <vjzm44@naver.com>
2026-06-08 10:15:30 +0900

    로깅 임시경로추가

--- src/Vamserlike.Api/Controllers/HealthController.cs
+++ src/Vamserlike.Api/Controllers/HealthController.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+using Vamserlike.Api.Configurations;
 using Vamserlike.Api.Dtos.Common;
+using Vamserlike.Api.Repositories;
+using Vamserlike.Api.Services;
 
 namespace Vamserlike.Api.Controllers;
 
@@ -7,9 +12,20 @@ namespace Vamserlike.Api.Controllers;
 [Route("api/[controller]")]
 public class HealthController : ControllerBase
 {
+    private readonly ILogger<HealthController> _logger;
+
+    public HealthController(
+        ILogger<HealthController> logger)
+    {
+        _logger = logger;
+    }
+
+
     [HttpGet]
     public ActionResult<ApiResponse<object>> Get()
     {
+        _logger.LogInformation("HealthCheck Ok.....");
+
         return Ok(ApiResponse<object>.Ok(new
         {
             Status = "ok",
